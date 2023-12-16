5c2fa3ff4d85721f66de1b427557f6df586a7584
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2023-12-16 20:55:44 +0700

    Made wander state for enermy

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] [Range(0f, 2f)] private float smooth;
+    private Vector3 offset;
+
+
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = new Vector3(0f, 0f, -10f);
+        transform.position = target.position + offset;
+    }
+    private void FixedUpdate()
+    {
+        FollowTarget();
+    }
+
+
+    private void FollowTarget()
+    {
+        Vector3 targetPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, smooth * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Enermy/FSM/Actions/ActionWander.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionWander : FSMAction
+{
+
+    [SerializeField] private Vector2 moveRange;
+    [SerializeField][Range(1f, 10f)] float moveSpeed;   
+    [SerializeField][Range(1f,10f)] private float timeWander;
+
+    private float timer = 0f;
+    private Vector3 movePosition;
+
+    private void Start()
+    {
+        timer = timeWander;
+        GetNewPosDestination();
+    }
+    public override void Action()
+    {
+        
+        timer -= Time.deltaTime;
+        Movement();
+        if(timer <= 0f)
+        {
+            timer = timeWander;
+            GetNewPosDestination();
+        }
+       
+    }
+
+    private void Movement()
+    {
+        Vector3 moveDirection = (movePosition - transform.position).normalized;
+        Vector3 movement = moveDirection * moveSpeed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, movePosition) >= 0.05f)
+        {
+            transform.Translate(movement);
+        }
+    }
+
+    
+
+    private void GetNewPosDestination()
+    {
+        float randomX = Random.Range(-moveRange.x, moveRange.x) * 1.5f;
+        float randomY = Random.Range(-moveRange.y, moveRange.y) * 1.5f;
+        movePosition = transform.position + new Vector3(randomX, randomY);
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, moveRange *2f);
+        Gizmos.DrawLine(transform.position, movePosition);
+    }
+
+}
