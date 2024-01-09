936ca4862642a57618242679796b670871c150e6
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2024-01-09 20:41:53 +0700

    Code feature stack item in inventory

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -26,43 +26,34 @@ public class Inventory : Singleton<Inventory>
         
     }
 
-    private void AddItem(Items item,int amount)
+
+
+
+    private void AddItem(Items item, int amount)
     {
-        
         if (item == null || amount <= 0) return;
-        List<int> itemsIndexes = CheckItemStock(item.ID);
-        if (item.isStackable && itemsIndexes.Count > 0)
+        List<int> itemIdexes = FindItemsStock(item.ID);
+        if(item.isStackable && itemIdexes.Count > 0)
         {
-            for (int i = 0; i < itemsIndexes.Count; i++)
+            foreach(int index in itemIdexes)
             {
                 int maxStack = item.maxStack;
-                if (inventoryItems[i].amountItem < maxStack)
+                int spaceAvailable = maxStack - inventoryItems[index].amountItem;
+                if(spaceAvailable > 0)
                 {
-                    inventoryItems[i].amountItem += amount;
-                    if (inventoryItems[i].amountItem > maxStack)
-                    {
-                        int dif = inventoryItems[i].amountItem - maxStack;
-                        inventoryItems[i].amountItem = maxStack;
-                        AddItem(item, dif);
-                    }
-                    InventoryUI.Instance.DrawSlot(inventoryItems[i], i);
-                    return;
+                    int amountToAdd = Mathf.Min(spaceAvailable, amount);
+                    inventoryItems[index].amountItem += amountToAdd;
+                    InventoryUI.Instance.DrawSlot(inventoryItems[index], index);
+                    amount -= amountToAdd;
+                    if (amount <= 0) return;
                 }
             }
         }
-        int amountAdd = amount > item.maxStack ? item.maxStack : amount;
-        AddItemsInFreeSlot(item, amountAdd);
-        int remainder = amount - amountAdd;
-        if(remainder > 0)
-        {
-            AddItem(item, remainder);   
-        }
-
+        AddItemInFreeSlot(item, amount);
     }
 
 
-
-    private void AddItemsInFreeSlot(Items item,int amount)
+    private void AddItemInFreeSlot(Items item,int amount)
     {
         for(int i = 0; i < inventorySize; i++)
         {
@@ -73,17 +64,20 @@ public class Inventory : Singleton<Inventory>
             return;
         }
     }
-
-
-    private List<int> CheckItemStock(string ID)
+    
+    private List<int> FindItemsStock(string ID)
     {
         List<int> result = new List<int>();
-        for(int i = 0; i < inventorySize; i++)
+
+        for (int i = 0; i < inventorySize;i++)
         {
             if (inventoryItems[i] == null) continue;
             if(inventoryItems[i].ID == ID) result.Add(i);
         }
+
         return result;
     }
+
+    
 
 }
