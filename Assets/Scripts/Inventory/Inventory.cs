28a4dd6c9b0a84f67c841179a201bf6a7a0855d5
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2024-01-09 19:45:38 +0700

    fix feature stackable items in inventory

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -6,7 +6,7 @@ public class Inventory : Singleton<Inventory>
 {
     [SerializeField] private Items itemTest;
     [SerializeField] private int inventorySize;
-    [SerializeField] private Items[] items;
+    [SerializeField] private Items[] inventoryItems;
 
     public int InventorySize => inventorySize;
 
@@ -14,69 +14,76 @@ public class Inventory : Singleton<Inventory>
     protected override void Awake()
     {
         base.Awake();
-        items = new Items[inventorySize];
+        inventoryItems = new Items[inventorySize];
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            /*items[0] = itemTest.CopyItem();
-            items[0].amountItem = 20;
-            InventoryUI.Instance.DrawSlot(items[0], 0);*/
-            
             AddItem(itemTest, 5);
-
-
         }
         
     }
 
-
-    private void AddItem(Items item, int amount)
+    private void AddItem(Items item,int amount)
     {
-        List<int> stocks = CheckItemsStock(item.ID);
-        if(item.isStackable || stocks.Count > 0)
+        
+        if (item == null || amount <= 0) return;
+        List<int> itemsIndexes = CheckItemStock(item.ID);
+        if (item.isStackable && itemsIndexes.Count > 0)
         {
-            foreach(int iStock in stocks)
+            for (int i = 0; i < itemsIndexes.Count; i++)
             {
-                int maxStack = items[iStock].maxStack;
-                if(items[iStock].amountItem < maxStack)
+                int maxStack = item.maxStack;
+                if (inventoryItems[i].amountItem < maxStack)
                 {
-                    items[iStock].amountItem += amount;
-                    if(items[iStock].amountItem > maxStack)
+                    inventoryItems[i].amountItem += amount;
+                    if (inventoryItems[i].amountItem > maxStack)
                     {
-                        int dif = items[iStock].amountItem - maxStack;
+                        int dif = inventoryItems[i].amountItem - maxStack;
+                        inventoryItems[i].amountItem = maxStack;
+                        AddItem(item, dif);
                     }
+                    InventoryUI.Instance.DrawSlot(inventoryItems[i], i);
+                    return;
                 }
             }
         }
+        int amountAdd = amount > item.maxStack ? item.maxStack : amount;
+        AddItemsInFreeSlot(item, amountAdd);
+        int remainder = amount - amountAdd;
+        if(remainder > 0)
+        {
+            AddItem(item, remainder);   
+        }
+
     }
 
-    private void AddItemInFreeSlot(Items item,int amount)
+
+
+    private void AddItemsInFreeSlot(Items item,int amount)
     {
-        for(int i = 0; i < items.Length; i++)
+        for(int i = 0; i < inventorySize; i++)
         {
-            if (items[i] != null) continue;
-            if (items[i] == null)
-            {
-                items[i] = itemTest.CopyItem();
-                items[i].amountItem = amount;
-                return;
-            }
+            if (inventoryItems[i] != null) continue;
+            inventoryItems[i] = item.CopyItem();
+            inventoryItems[i].amountItem = amount;
+            InventoryUI.Instance.DrawSlot(inventoryItems[i], i);
+            return;
         }
     }
 
-    private List<int> CheckItemsStock(string idItem)
+
+    private List<int> CheckItemStock(string ID)
     {
-        List<int> indexes = new List<int>();
-        
-        for(int i = 0; i < items.Length; i++)
+        List<int> result = new List<int>();
+        for(int i = 0; i < inventorySize; i++)
         {
-            if (items[i] == null) continue;
-            if (items[i].ID == idItem) indexes.Add(i);
+            if (inventoryItems[i] == null) continue;
+            if(inventoryItems[i].ID == ID) result.Add(i);
         }
-        return indexes;
+        return result;
     }
-       
+
 }
