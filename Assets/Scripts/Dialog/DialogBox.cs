bf034747465ce6696faa7800eba6f459b42996b9
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2024-01-30 16:26:44 +0700

    Add code Dialog Box

---
+++ Assets/Scripts/Dialog/DialogBox.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogBox : MonoBehaviour
+{
+
+    [SerializeField] private Image avartarCharactor;
+    [SerializeField] private string nameCharactor;
+    [SerializeField] private string[] listDialog;
+    public void LoadDialogBox(Dialog dialogBox)
+    {
+        
+    }
+}
