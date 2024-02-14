c4c98ec923d55ffaf11185338082d28755d1cd4f
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2024-02-14 20:41:04 +0700

    Add quest config

---
+++ Assets/Scripts/Quest/Quest.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[CreateAssetMenu(fileName ="Quest",menuName ="Quest SO")]
+public class Quest : ScriptableObject
+{
+    [Header("Config")]
+    public string questName;
+    public string IDQuest;
+    [TextArea] public string Description;
+
+
+    [Header("Reward")]
+    public int goldReward;
+    public int expReward;
+    public ItemReward itemReward;
+
+
+    [HideInInspector] public int currentQuestStatus;
+    [HideInInspector] public bool questCompleted;
+    [HideInInspector] public bool questAccepted;
+}
+
+
+
+[Serializable]
+public class ItemReward
+{
+    public Items itemReward;
+    public int itemAmount;
+
+}
